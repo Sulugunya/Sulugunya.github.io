cf6b57b3af5de387ca7d8b7039f9ff7599b2126c
Panteleev <panvey@yandex.ru>
2021-05-01 19:40:15 +0400

    intit commit

--- News Blog/js/index.js
+++ News Blog/js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     $(document).ready(function(){
         $.get(
-            'http://data.fixer.io/api/latest',
+            'https://data.fixer.io/api/latest',
             {'access_key': '523ad70b2f0e3829b5f5f8dfae5dacc8'},
             function(response){
                 $('.rate-list').prepend(`
